fb06f954cfdded6657d39f3c2900c10cfc326595
Erkan Ertürk <erkanerturk096@gmail.com>
2019-03-06 12:01:12 +0300

    CORS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const movieRouter = require("./routes/movie");
@@ -24,6 +25,7 @@ const verifyToken = require("./middleware/verify-token");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
